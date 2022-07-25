08e0bbf00a3b45a89a9580e28675f8b498f4d78a
Taylor Savage <taylorsavage93@gmail.com>
2022-07-25 15:33:00 -0400

    finished exercise 2

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,26 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("mom", true)]
+        [InlineData("Friend", false)]
+        [InlineData("dad", true)]
+        public void MyTest(string word, bool expected)
         {
-
+            
+            {
+                //arrange
+                var test = new WordSmith();
+                //act
+                var actual = test.IsAPalindrome(word);
+                //assert
+                Assert.Equal(expected, actual);
+            }
         }
     }
 }
